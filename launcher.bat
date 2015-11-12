bf7604ca8e3dd72fb205d4f5ea7ec68f3284ecd5
Yoan Malié <malie.yoan@gmail.com>
2015-11-12 23:26:07 +0100

    First version

---
+++ launcher.bat
@@ -0,0 +1,23 @@
+:: Don't show tasks in the command prompt
+@echo off
+
+:: Title of the command prompt
+title "Worktools launcher"
+
+:: Project & apps directory
+set project="C:\wamp\www\myproject"
+set sublimetext="C:\Program Files\Sublime Text 3" sublime_text.exe
+set wamp="C:\wamp" wampmanager.exe
+
+:: Open directory
+%SystemRoot%\explorer.exe %project%
+
+:: Run Apps
+start /d %sublimetext%
+start /d %wamp%
+
+:: Pointing to project directory
+cd %project%
+
+:: Git commands
+cmd /k git status
